320aae782f6bd1cc522772ec2cd535acbf416e20
vilarinux <davi.r.vilarinho@gmail.com>
2021-10-12 14:36:51 -0300

    monitors now automatically config without error + gamma. Backup dotfiles created

---
+++ backup-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -f ~/.zshrc ~/riceBin/zshrc
+cp -f ~/.vimrc ~/riceBin/vimrc
+cp -f ~/.tmux.conf ~/riceBin/tmux.conf
+cp -f ~/.config/bspwm/bspwmrc ~/riceBin/bspwm/bspwmrc
+cp -f ~/.config/sxhkd/sxhkdrc ~/riceBin/bspwm/sxhkdrc
+cp -f ~/.config/dunst/dunstrc ~/riceBin/bspwm/dunstrc
